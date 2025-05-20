0dc45c157ff6539ecbf6e2e991e82233d0415e1d
李嘉图·M·路 <146103794+Ricardo-M-Zheng@users.noreply.github.com>
2025-05-21 01:30:50 +0800

    init work

---
+++ src/mod.ts
@@ -0,0 +1,27 @@
+import { initTailwind } from "./compiler.ts";
+import type { FreshBuilder } from "fresh/dev";
+import type { App } from "fresh";
+import type { TailwindPluginOptions } from "./types.ts";
+
+export function tailwind<T>(
+  builder: FreshBuilder,
+  app: App<T>,
+  options: TailwindPluginOptions = {},
+): void {
+  let processor: ReturnType<typeof initTailwind> | null = null;
+
+  builder.onTransformStaticFile(
+    { pluginName: "tailwind", filter: /\.css$/, exclude: options.exclude },
+    async (args) => {
+      if (!processor) processor = initTailwind(app.config);
+      const instance = await processor;
+      const res = await instance.process(args.text, {
+        from: args.path,
+      });
+      return {
+        content: res.content,
+        map: res.map?.toString(),
+      };
+    },
+  );
+}
